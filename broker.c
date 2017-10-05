5953a0e82bec780a1681b75bb87cfb596ef7c2bf
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-10-05 00:35:21 -0600

    Implement basic 1to1 subscription + tests

--- broker.c
+++ broker.c
@@ -16,5 +16,45 @@ broker_context_t* broker_init(broker_subscriber_t* subscribers_array, uint32_t s
 
     memset(subscribers_array, 0, sizeof(broker_subscriber_t) * subscribers_array_length);
     m_context.subscribers = subscribers_array;
+    m_context.subscribers_length = subscribers_array_length;
+    m_context.initialized = true;
     return &m_context;
+}
+
+// TODO: Allow multiple topics per listener, or multiple listeners per topic?
+bool broker_subscribe(broker_context_t* context, broker_topic_t topic, broker_listener_fn_t listener)
+{
+    if(context == NULL || listener == NULL)
+    {
+        return false;
+    }
+
+    if(!context->initialized)
+    {
+        return false;
+    }
+
+    for(uint32_t i = 0; i < context->subscribers_length; i++)
+    {
+        if(context->subscribers[i].listener == NULL)
+        {
+            // Subscribe
+            context->subscribers[i].topic = topic;
+            context->subscribers[i].listener = listener;
+            return true;
+        }
+        else if(context->subscribers[i].listener == listener && context->subscribers[i].topic == topic)
+        {
+            // Already subscribed
+            return true;
+        }
+    }
+
+    // Max number of subscribers reached
+    return false;
+}
+
+bool broker_publish(broker_context_t* context, broker_topic_t topic, void* event)
+{
+    return false;
 }
--- broker.h
+++ broker.h
@@ -12,20 +12,22 @@
 
 // Types
 typedef uint32_t broker_topic_t;
-typedef void (*broker_listener_t)(void*);
+typedef void (*broker_listener_fn_t)(void*);
 
 typedef struct
 {
-    broker_topic_t    topic;
-    broker_listener_t broker_listener;
+    broker_topic_t       topic;
+    broker_listener_fn_t listener;
 } broker_subscriber_t;
 
 typedef struct
 {
     broker_subscriber_t* subscribers;
+    uint32_t             subscribers_length;
+    bool                 initialized;
 } broker_context_t;
 
 // Prototypes
 broker_context_t* broker_init(broker_subscriber_t* subscribers_array, uint32_t subscribers_array_length);
-bool broker_subscribe(broker_topic_t topic, broker_listener_t broker_listener);
-bool broker_publish(broker_topic_t topic, void* event);
+bool broker_subscribe(broker_context_t* context, broker_topic_t topic, broker_listener_fn_t listener);
+bool broker_publish(broker_context_t* context, broker_topic_t topic, void* event);
