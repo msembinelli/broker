2dc33423188a7e06fa6e9725a0a74059b009ff6a
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-10-05 13:07:55 -0600

    Refactor for multiple subscribers per topic model
    TODO: Update tests

--- broker.c
+++ broker.c
@@ -6,55 +6,110 @@
 // Types
 static broker_context_t m_context;
 
-broker_context_t* broker_init(broker_subscriber_t* subscribers_array, uint32_t subscribers_array_length)
+broker_context_t* broker_init(broker_topic_t* max_topics, uint32_t max_topics_length)
 {
-    if(subscribers_array_length == 0 || subscribers_array == NULL)
+    if(max_topics_length == 0 || max_topics == NULL)
     {
         // No static subscriber memory allocated
         return NULL;
     }
 
-    memset(subscribers_array, 0, sizeof(broker_subscriber_t) * subscribers_array_length);
-    m_context.subscribers = subscribers_array;
-    m_context.subscribers_length = subscribers_array_length;
+    for(uint32_t i = 0; i < max_topics_length; i++)
+    {
+        if(max_topics[i]->listeners == NULL || max_topics[i]->listeners_length == 0)
+        {
+            // Topic has no listener static allocation
+            return NULL;
+        }
+        memset(max_topics[i]->listeners, 0, sizeof(broker_listener_fn_t) * max_topics[i]->listeners_length);
+    }
+
+    m_context.topics = max_topics;
+    m_context.topics_length= max_topics_length;
     m_context.initialized = true;
     return &m_context;
 }
 
 // TODO: Allow multiple listeners per topic
-bool broker_subscribe(broker_context_t* context, broker_topic_t topic, broker_listener_fn_t listener)
+bool broker_subscribe(broker_context_t* context, broker_topic_index_t topic_index, broker_listener_fn_t listener)
 {
     if(context == NULL || listener == NULL)
     {
+        // TODO: error -> NULL_PARAMETER
         return false;
     }
 
-    if(!context->initialized)
+    if(!context->initialized || context->topics == NULL)
     {
+        // TODO: error -> CONTEXT_NOT_INITIALIZED
         return false;
     }
 
-    for(uint32_t i = 0; i < context->subscribers_length; i++)
+    if(topic_index >= context->topics_length)
     {
-        if(context->subscribers[i].listener == NULL)
+        // TODO: error -> TOPIC_OUT_OF_RANGE
+        return false;
+    }
+
+    // TODO: Improve this search...can we use hash tables?
+    for(uint32_t i = 0; i < context->topics[topic_index]->listeners_length; i++)
+    {
+        if(context->topics[topic_index]->listeners[i] == listener)
+        {
+            // Already subscribed to this topic
+            // TODO: return -> SUBSCRIBE_SUCCESS
+            return true;
+        }
+    }
+
+    for(uint32_t i = 0; i < context->topics[topic_index]->listeners_length; i++)
+    {
+        if(context->topics[topic_index]->listeners[i] == NULL)
         {
             // Subscribe
-            context->subscribers[i].topic = topic;
-            context->subscribers[i].listener = listener;
-            return true;
-        }
-        else if(context->subscribers[i].listener == listener && context->subscribers[i].topic == topic)
-        {
-            // Already subscribed
+            context->topics[topics_index]->listeners[i] = listener;
+            // TODO: return -> SUBSCRIBE_SUCCESS
             return true;
         }
     }
 
-    // Max number of subscribers reached
+    // Max number of topics reached
+    // TODO: error -> MAX_NUMBER_SUBSCRIBERS_PER_TOPIC_REACHED
     return false;
 }
 
-bool broker_publish(broker_context_t* context, broker_topic_t topic, void* event)
+bool broker_publish(broker_context_t* context, broker_topic_index_t topic_index, void* event)
 {
+
+    if(context == NULL || event == NULL)
+    {
+        // TODO: error -> NULL_PARAMETER
+        return false;
+    }
+
+    if(!context->initialized || context->topics == NULL)
+    {
+        // TODO: error -> CONTEXT_NOT_INITIALIZED
+        return false;
+    }
+
+    if(topic_index >= context->topics_length)
+    {
+        // TODO: error -> TOPIC_OUT_OF_RANGE
+        return false;
+    }
+
+    // Fire all event listeners for the topic sequentially
+    for(uint32_t i = 0; i < context->topics[topic_index]->listeners_length; i++)
+    {
+        if(context->topics[topic_index]->listeners[i] != NULL)
+        {
+            context->topics[topic_index]->listener[i](event);
+            // TODO: return -> PUBLISH_SUCCESS
+            return true;
+        }
+    }
+
+    // TODO: error -> PUBLISH_FAILED
     return false;
 }
--- broker.h
+++ broker.h
@@ -2,32 +2,29 @@
 
 // Includes
 #include "stdint.h"
-#include "stdbool.h"
-#include "stddef.h"
 #include "stdlib.h"
 #include "string.h"
 
 // Macros
-#define ARRAY_LENGTH(x) sizeof(x)/sizeof(x[0])
 
 // Types
-typedef uint32_t broker_topic_t;
-typedef void (*broker_listener_fn_t)(void*);
+typedef uint32_t broker_topic_index_t;
+void (*broker_listener_fn_t)((void*));
 
 typedef struct
 {
-    broker_topic_t       topic;
-    broker_listener_fn_t listener;
-} broker_subscriber_t;
+    broker_listener_fn_t* listeners;
+    uint32_t*             listeners_length;
+} broker_topic_t;
 
 typedef struct
 {
-    broker_subscriber_t* subscribers;
-    uint32_t             subscribers_length;
-    bool                 initialized;
+    broker_topic_t*       topics;
+    uint32_t              topics_length;
+    boolean               initialized;
 } broker_context_t;
 
 // Prototypes
-broker_context_t* broker_init(broker_subscriber_t* subscribers_array, uint32_t subscribers_array_length);
-bool broker_subscribe(broker_context_t* context, broker_topic_t topic, broker_listener_fn_t listener);
-bool broker_publish(broker_context_t* context, broker_topic_t topic, void* event);
+broker_context_t* broker_init(broker_topic_t* max_topics, uint32_t max_topics_length);
+boolean broker_subscribe(broker_context_t* context, broker_topic_index_t topic_index, broker_listener_fn_t listener);
+boolean broker_publish(broker_context_t* context, broker_topic_index_t topic_index, (void*)event);
